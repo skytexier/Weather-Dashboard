5cd0a3f4852783c09c0e762c17e877bc4e126813
Sky Texier <skyhamilton@gmail.com>
2022-09-07 17:34:27 -0700

    Updated script to include API that can catch UV Index, and better properly display weather data. Previous API still used to call certain citys based upon name. Some minor styling changes

--- Assets/Javascript/script.js
+++ Assets/Javascript/script.js
@@ -7,52 +7,72 @@ var currentDay = document.querySelector('#daily');
 // Function to call city, get API based on city name through concatenation 
 var callCity = function (cityname) {
 var apiURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityname + "&appid=825f8c44f819c519c3d1ebb993400003&units=imperial";
+
+//Creating variables to store different API calls
+var currentCity, onecall;
+
+//Fetching first API
 fetch(apiURL)
 .then(function (response){
     return response.json();
 })
 .then(function (data) {
     console.log(data);
-    
-    //Naming our weather box
-    var actualName = data.city.name;
+    //Getting Lat/Lon from this API
+    var cityLat = data.city.coord.lat;
+    var cityLon = data.city.coord.lon;
+    //Assigning this API's data to a variable
+    currentCity = data;
+    //Requesting another variable (not necessary as I could just use one but for experience gains I'm using two)
+    return fetch("https://api.openweathermap.org/data/3.0/onecall?lat=" + cityLat + "&lon=" + cityLon + "&exclude=minutely,hourly&appid=825f8c44f819c519c3d1ebb993400003&units=imperial")
+})
+.then(function (response){
+        return response.json();
+})
+.then (function (data){
+    console.log(data);
+    //Assigning the second API to a variable
+    onecall = data;
 
-    //Converting unix time
-   var currentDate = new Date(data.list[0].dt * 1000);
+    //Naming our weather box  from our first API
+    var actualName = currentCity.city.name;
+
+    //Converting the onecall date from unix time
+   var currentDate = new Date(onecall.current.dt * 1000);
    var currentDateText = currentDate.toLocaleDateString("en-US");
 
 
 
     //Getting icon codes/url
-    var iconcode = data.list[0].weather[0].icon
+    var iconcode = onecall.current.weather[0].icon
     var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
 
-    //Setting main display box
+    //Setting main display box to display proper name and proper date
     $(cityNameEl).text(actualName + " (" + currentDateText + ")");
     $('#weathericon').attr("src", iconurl);
 
     //Empty all previous data and append new
     $(currentDay).empty()
-    $(currentDay).append("<p> Temp: " + data.list[0].main.temp + "°F </p>");
-    $(currentDay).append("<p> Wind: " + data.list[0].wind.speed + " MPH </p>");
-    $(currentDay).append("<p> Humidity: " + data.list[0].main.humidity + "</p>");
-    $(currentDay).append("<p> UV Index: " + data.list[0].main.temp + "</p>");
+    $(currentDay).append("<p> Temp: " + onecall.current.temp + "°F </p>");
+    $(currentDay).append("<p> Wind: " + onecall.current.wind_speed + " MPH </p>");
+    $(currentDay).append("<p> Humidity: " + onecall.current.humidity + "</p>");
+    $(currentDay).append("<p> UV Index: " + onecall.current.uvi + "</p>");
 
-
-
-    for (let i = 1; i < data.list.length; i+=8) {
+    //For loop to iterate through 5 day forecast
+    $(".fivedaycard").remove()
+    for (let i = 1; i < 6; i++) {
         var dayId= "#" + i;
-        var formatDate = new Date(data.list[i].dt * 1000);
+        var formatDate = new Date(onecall.daily[i].dt * 1000);
         var dateEl = formatDate.toLocaleDateString("en-Us");
 
 
         $(fivedayEl).append("<div class='fivedaycard' id= "+ i + "></div>");
         $(dayId).append("<h3>" + dateEl + "</h3>");
-        $(dayId).append("<img src=http://openweathermap.org/img/w/" + data.list[i].weather[0].icon + ".png </img>");
-        $(dayId).append("<p> Temp: " + data.list[i].main.temp + "°F </p>");
-        $(dayId).append("<p> Wind: " + data.list[i].wind.speed + " MPH </p>");
-        $(dayId).append("<p> Humidity: " + data.list[i].main.humidity + "</p>");
-        $(dayId).append("<p> UV Index: " + data.list[i].main.temp + "</p>");
+        $(dayId).append("<img src=http://openweathermap.org/img/w/" + onecall.daily[i].weather[0].icon + ".png </img>");
+        $(dayId).append("<p> Temp: " + onecall.daily[i].temp.day + "°F </p>");
+        $(dayId).append("<p> Wind: " + onecall.daily[i].wind_speed + " MPH </p>");
+        $(dayId).append("<p> Humidity: " + onecall.daily[i].humidity + "</p>");
+        $(dayId).append("<p> UV Index: " + onecall.daily[i].uvi + "</p>");
     };
 
   
@@ -73,5 +93,5 @@ var buttonClickHandler = function (event) {
     }
 
 };
-
+//Search button on click
 buttonEl.addEventListener("click", buttonClickHandler);
